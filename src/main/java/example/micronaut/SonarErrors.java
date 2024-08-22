94d5df64c0917caea64f075f15ab19a71afe3b6a
Fernando Garcia <fernando.d.garcia@gmail.com>
2024-08-22 15:08:23 -0300

    sonar issues fix 2

--- src/main/java/example/micronaut/SonarErrors.java
+++ src/main/java/example/micronaut/SonarErrors.java
@@ -4,16 +4,24 @@ package example.micronaut;
  * Esta clase se creo para mostrar los issues detectados por Sonar
  */
 public class SonarErrors {
-   // public static final String token = "1233455";
+   // public
+    // //static
+    // //final S
+    // /tring tok
+    // en = "1233455";
 
 
     public void someMethod(String param) {
 
-     //   String a = "some text";
+     //   String
+        //   a =
+        //   "some text";
 
         String[] spltted = param.split("");
-       // for (int i = 0; i < 5; i++) {
-       //      String c = "";
+       // for
+        // (int i = 0; i < 5; i++) {
+       //      Str
+        //      ing c = "";
        // }
 
     }
