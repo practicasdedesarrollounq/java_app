a80715534a40ea361d2421e63b5dca33b1e81432
Fernando Garcia <fernando.d.garcia@gmail.com>
2024-08-22 15:00:19 -0300

    sonar tests

---
+++ src/main/java/example/micronaut/SonarErrors.java
@@ -0,0 +1,16 @@
+package example.micronaut;
+
+public class SonarErrors {
+    public static final String token = "1233455";
+
+
+    public void someMethod(String param) {
+
+        String a = "some text";
+
+        for (int i = 0; i < 5; i++) {
+            String c = "";
+        }
+
+    }
+}
