1a0b46b693d7f1783ac3f18e1b22f8b3e37db9fd
Fernando Garcia <fernando.d.garcia@gmail.com>
2025-10-02 18:54:31 -0300

    errores en el feature

---
+++ src/main/java/example/micronaut/Service.java
@@ -0,0 +1,12 @@
+package example.micronaut;
+
+public class Service {
+
+    public void METODO(){
+        String a;
+        String b = "123";
+        String c = "";
+        String z =  a + b;
+
+    }
+}
