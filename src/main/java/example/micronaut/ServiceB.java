2175900aaea8f51181e7a08da4e99dcbeb8d0dff
Fernando Garcia <fernando.d.garcia@gmail.com>
2025-10-02 21:12:23 -0300

    errores en el feature

---
+++ src/main/java/example/micronaut/ServiceB.java
@@ -0,0 +1,51 @@
+package example.micronaut;
+
+public class ServiceB {
+
+    public void METODO(){
+        String A = "";
+        String b = "123";
+        String c = "";
+        String z =  A + b;
+
+
+        int zero = 0;
+        int a = 123;
+        var b = a / zero;
+    }
+
+    public void calculo(){
+
+        int zero = 0;
+        int a = 123;
+        var b = a / zero;
+
+        System.out.println("tirar los logs a sys out no es muy bueno" + b);
+    }
+
+
+    //@SuppressWarnings("java:S106")
+    public void otroMetodo() {
+        System.out.println("tirar los logs a sys out no es muy bueno");
+
+    }
+
+    //@SuppressWarnings("java:S106")
+    public void loop() {
+        int x = 100;
+        for (int i = 0; i < x; i++) {
+            System.out.println("loop");
+            x = i + 2;
+        }
+
+    }
+
+
+
+/*
+    public void codigoComentado() {
+        System.out.println("tirar los logs a sys out no es muy bueno");
+
+    }
+*/
+}
